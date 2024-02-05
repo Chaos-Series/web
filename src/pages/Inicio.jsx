950d98df9ca31057455d1f956f6754e39b13a522
Zas <thezasm03@gmail.com>
2024-02-05 13:48:21 +0100

    Creacion minicalendario y update de endpoint

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,9 +1,32 @@
-import MainLayout from "../layout/MainLayout";
-
-import { Button } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { Button, Spinner } from "@nextui-org/react";
 import { Link } from "react-router-dom";
 
+import { conseguirPartidos } from "../services/partidos";
+
+import MainLayout from "../layout/MainLayout";
+
 function Inicio() {
+  const [partidos, setPartidos] = useState();
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    conseguirPartidos().then((listaJugadores) => {
+      setPartidos(listaJugadores.result);
+      setCargando(false);
+    });
+  }, []);
+
+  if (cargando) {
+    return (
+      <MainLayout>
+        <div className="flex flex-wrap justify-center items-center m-4 gap-4 min-h-[296px]">
+          <Spinner label="Cargando..." size="lg" />
+        </div>
+        </MainLayout>
+    );
+  }
+
   return (
     <MainLayout>
       <div className="bg-[url('../assets/images/Inscripciones.jpg');] bg-no-repeat bg-cover bg-center">
