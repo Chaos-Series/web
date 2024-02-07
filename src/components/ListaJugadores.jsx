833d7c8956ea17678cbbabd7395cb2570464b50e
mateo <aleyt20@gmail.com>
2024-02-07 20:02:03 +0100

    ListaJugadores modified

--- src/components/ListaJugadores.jsx
+++ src/components/ListaJugadores.jsx
@@ -1,17 +1,9 @@
-import conseguirJugadores from "../services/usuarios";
+import MainLayoutInfo from "../layout/MainLayoutInfo";
+
+import { conseguirJugadores } from "../services/usuarios";
 
 export default function ListaJugadores({ conseguirJugadores }){
 
-  const [jugadores, setJugadores] = useState();
-  const [cargando, setCargando] = useState(true);
-
-  useEffect(() => {
-    conseguirJugadores().then((listaJugadores) => {
-      setJugadores(listaJugadores.result);
-      setCargando(false);
-    });
-  }, []);
-  
     return (
         <MainLayoutInfo texto={"Draft"}>
       <div className="flex flex-wrap justify-center items-center m-4 gap-4">
